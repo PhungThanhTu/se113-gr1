cc29bc709461573a689c15ea0516892e87b01e79
phungthanhtu <19522455@gm.uit.edu.vn>
2021-12-08 23:59:36 +0700

    reuse old code

--- src/Student-Enroll-REAL/SQLInteraction/CheckLogin.cs
+++ src/Student-Enroll-REAL/SQLInteraction/CheckLogin.cs
@@ -42,35 +42,36 @@ namespace Student_Enroll_REAL.SQLInteraction
             try
             {
                 connection.Open();
+                cmd.CommandText = "Select * from TAI_KHOAN_DANG_NHAP "
+                            + "where ten_tai_khoan = '" + username + "' "
+                            + "and mat_khau = '" + password + "'";
+           
+
+                cmd.Connection = connection;
+                reader = cmd.ExecuteReader();
+
+                int i = 0;
+                bool isAdmin = false;
+
+                while(reader.Read())
+                {
+                    i++;
+                    isAdmin = (bool)reader["admin"];
+                }
+                cmd.Connection.Close();
+                if(i != 0 && !isAdmin)
+                    return "user";
+                if (i != 0 && isAdmin)
+                    return "admin";
+                else return "failed";
+
             }
             catch
             {
                 return "error";
             }
 
-            cmd.CommandText = "Select * from TAI_KHOAN_DANG_NHAP "
-                            + "where ten_tai_khoan = '" + username + "' "
-                            + "and mat_khau = '" + password + "'";
-           
-
-            cmd.Connection = connection;
-            reader = cmd.ExecuteReader();
-
-            int i = 0;
-            bool isAdmin = false;
-
-            while(reader.Read())
-            {
-                i++;
-                isAdmin = (bool)reader["admin"];
-            }
-            cmd.Connection.Close();
-            if(i != 0 && !isAdmin)
-                return "user";
-            if (i != 0 && isAdmin)
-                return "admin";
-            else return "failed";
-
+            
         }
 
         public static SqlConnection Connection { get { return connection; } }
--- src/Student-Enroll-REAL/SQLInteraction/SQLConnector.cs
+++ src/Student-Enroll-REAL/SQLInteraction/SQLConnector.cs
@@ -7,12 +7,11 @@ using System.Windows;
 
 namespace Student_Enroll_REAL.SQLInteraction
 {
-
+    
 
     class SQLConnector
     {
 
-        public static string ConnectionString = "Data Source = SQL5105.site4now.net; Initial Catalog = db_a7d89c_phungthanhtu2; User Id = db_a7d89c_phungthanhtu2_admin; Password=Phungthanhtu!1";
-
+    public static string ConnectionString = "Data Source=SQL5105.site4now.net;Initial Catalog=db_a7d89c_phungthanhtu2;User Id=db_a7d89c_phungthanhtu2_admin;Password=Phungthanhtu!1";
     }
 }
